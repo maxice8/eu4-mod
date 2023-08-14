1e0e778be28ab5bd2676f9dddc0394ecb5dd6b60
maxice8 <thinkabit.ukim@gmail.com>
2023-08-14 06:55:01 +0000

    abstract ZIP_PATH into a variable

--- mkzip.sh
+++ mkzip.sh
@@ -3,6 +3,8 @@
 : "${ZIP_NAME:=$MOD_NAME}"
 : "${PREFIX_NAME:=$MOD_NAME}"
 
+ZIP_PATH="$ZIP_TMPDIR/mod/$ZIP_NAME.zip"
+
 # Create directory for the mod
 mkdir -p "$ZIP_TMPDIR"/mod
 mkdir -p "$ZIP_TMPDIR"/repo
@@ -13,7 +15,7 @@ git clone --depth 1 "$REPOSITORY" "$ZIP_TMPDIR"/repo
 # Create zip archive
 {
     cd "$ZIP_TMPDIR"/repo || exit 1
-    git archive -o "$ZIP_TMPDIR"/mod/"$ZIP_NAME".zip --prefix="$PREFIX_NAME"/ HEAD
+    git archive -o "$ZIP_PATH" --prefix="$PREFIX_NAME"/ HEAD
 }
 
 # Create descriptor
@@ -23,6 +25,6 @@ printf '\npath="%s"\n' "$PREFIX_NAME" >> "$ZIP_TMPDIR"/mod/"$MOD_NAME".mod
 
 # Add it to the zip, use -j to strip the paths so it only adds the file to the
 # root of the zip
-zip -u "$ZIP_TMPDIR/mod/$ZIP_NAME.zip" -j "$ZIP_TMPDIR"/mod/"$MOD_NAME".mod
+zip -u "$ZIP_PATH" -j "$ZIP_TMPDIR"/mod/"$MOD_NAME".mod
 
-echo "zip-path=$ZIP_TMPDIR/mod/$ZIP_NAME.zip" >> "$GITHUB_OUTPUT"
+echo "zip-path=$ZIP_PATH" >> "$GITHUB_OUTPUT"
