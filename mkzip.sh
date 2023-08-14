17aedf79641e6538d69d38d49887ef69130031ca
maxice8 <thinkabit.ukim@gmail.com>
2023-08-14 07:38:06 +0000

    revert

--- mkzip.sh
+++ mkzip.sh
@@ -4,20 +4,20 @@ set -eu
 : "${ZIP_NAME:=$MOD_NAME}"
 : "${PREFIX_NAME:=$MOD_NAME}"
 
-ZIP_PATH="$GITHUB_WORKSPACE/$ZIP_NAME.zip"
+ZIP_PATH="$GITHUB_WORKSPACE/mod/$ZIP_NAME.zip"
 
 # Create directory for the mod
-mkdir -p "$ZIP_TMPDIR"/
+mkdir -p "$ZIP_TMPDIR"/mod
+
+git archive -o "$ZIP_PATH" --prefix="$PREFIX_NAME"/ HEAD
 
 # Create descriptor
-cat descriptor.mod > "$ZIP_TMPDIR"/"$MOD_NAME".mod
+cat descriptor.mod > "$ZIP_TMPDIR"/mod/"$MOD_NAME".mod
 # Add PATH to descriptor
-printf '\npath="%s"\n' "$PREFIX_NAME" >> "$ZIP_TMPDIR"/"$MOD_NAME".mod
+printf '\npath="%s"\n' "$PREFIX_NAME" >> "$ZIP_TMPDIR"/mod/"$MOD_NAME".mod
 
-git archive \
-    -o "$ZIP_PATH" \
-    --prefix="$PREFIX_NAME"/ \
-    --add-file="$ZIP_TMPDIR/$MOD_NAME.mod" \
-    HEAD
+# Add it to the zip, use -j to strip the paths so it only adds the file to the
+# root of the zip
+zip -u "$ZIP_PATH" -j "$ZIP_TMPDIR"/mod/"$MOD_NAME".mod
 
 echo "zip-path=$ZIP_PATH" >> "$GITHUB_OUTPUT"
